2a015496880df4e8b1c64c756636c5c118a74cc2
Simonas <simposk@gmail.com>
2015-09-22 18:21:50 +0300

    Added post CRUD ability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,49 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
-	# def show
-	# end
+	def show
+	end
 
-	# def new
-	# end
+	def new
+		@post = Post.new
+	end
 
-	# def edit
-	# end
+	def create
+		@post = Post.new(post_params)
 
-	# def update
-	# end
+		if @post.save
+			redirect_to @post
+		else
+			render "new"
+		end
+	end
 
-	# def destroy
-	# end
+	def edit
+	end
 
-	# private
-	# 	def find_post
-	# 	end
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render "edit"
+		end
+	end
 
-	# 	def post_params
-	# 		params.require(:post).permit(:titlem, :link, :content)
-	# 	end
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+		def find_post
+			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :link, :description)
+		end
 end
